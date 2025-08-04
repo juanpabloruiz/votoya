ef2efd2278cb15dd343623b1a1a3cc283ed2d885
juanpabloruiz <pabloruiz1980@gmail.com>
2025-08-04 15:43:09 -0300

    Files deleted of node

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola mundo!\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Servidor corriendo en en http://${hostname}:${port}/`);
-});
+const { createServer } = require('http');
+const { readFile } = require('fs');
+const { join } = require('path');
+
+createServer((_, res) =>
+  readFile(join(__dirname, 'index.html'), (err, data) =>
+    err
+      ? res.writeHead(500).end('Error del servidor')
+      : res.writeHead(200, { 'Content-Type': 'text/html' }).end(data)
+  )
+).listen(3000, () => console.log('🟢 http://localhost:3000'));
